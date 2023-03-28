d4520bd79d8e3ed1f088ae1e99224b2701b7480a
Mónica Angulo-Bedoya <monica.angulo@fulbrightmail.org>
2023-03-27 20:17:25 -0500

    uploading initial website
    This is the first upload of all of the files needed for my website

---
+++ Scripts/responsive.js
@@ -0,0 +1,21 @@
+//  ==========================================================================
+// #Mobile Menu
+//  ========================================================================== 
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
