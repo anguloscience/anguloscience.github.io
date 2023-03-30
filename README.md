7f8ff04d69ec36f38bd5d4e14437acdfcf7f731c
anguloscience <monica.angulo@fulbrightmail.org>
2023-03-30 16:25:28 -0500

    Update v3

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # website
 Personal website with resume 
 
-Link Web statistics
-https://www.freecounterstat.com/geozoom.php?c=j4jzjn5dk8bmxqqbgl3wwt2lgeajgjuu&base=counter2
+<img a2.mp4>
+
+Counter stats: https://www.freecounterstat.com/geozoom.php?c=572u9aawueej3jjxf54qm3m8p8z7tns1&base=counter2&type_clic=1
+
+
--- Scripts/accordion.js
+++ Scripts/accordion.js
@@ -1,14 +1,8 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const accordion = document.getElementsByClassName('container');
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
 }
+
