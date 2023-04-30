d7fd85e32808e9c00700ba23beeef9d44c8e7d7f
anguloscience <monica.angulo@fulbrightmail.org>
2023-04-30 13:43:01 -0500

    Adding apps
    Updated the website and uploaded some apps that I have developed

---
+++ appPages/rock-paper/scripts/rock-paper.js
@@ -0,0 +1,170 @@
+let score = JSON.parse(localStorage.getItem("score")) || {
+   wins: 0,
+   losses: 0,
+   ties: 0,
+};
+/* Este if hace lo mismo que el || {} de arriba
+if (!score){
+  score = {
+     wins: 0,
+     losses: 0,
+     ties: 0
+  }
+}
+*/
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+   if (!isAutoPlaying) {
+      intervalId = setInterval(() => {
+         const playerMove = pickComputerMove();
+         playGame(playerMove);
+      }, 2000);
+      isAutoPlaying = true;
+   } else {
+      clearInterval(intervalId);
+      isAutoPlaying = false;
+   }
+}
+
+function stopAutoPlay() {
+   const autoPlayElement = document.querySelector(".js-autoplay-button");
+
+   if (autoPlayElement.innerText === "Auto play") {
+      autoPlayElement.innerHTML = "Stop auto play";
+   } else {
+      autoPlayElement.innerHTML = "Auto play";
+   }
+}
+
+document.querySelector(".js-rock-button").addEventListener("click", () => {
+   playGame("Rock");
+});
+
+document.querySelector(".js-paper-button").addEventListener("click", () => {
+   playGame("Paper");
+});
+
+document.querySelector(".js-scissors-button").addEventListener("click", () => {
+   playGame("Scissors");
+});
+
+document.querySelector(".js-lizard-button").addEventListener("click", () => {
+   playGame("Lizard");
+});
+
+document.querySelector(".js-spock-button").addEventListener("click", () => {
+   playGame("Spock");
+});
+
+document.body.addEventListener("keydown", (event) => {
+   if (event.key === "r") {
+      playGame("Rock");
+   } else if(event.key === "p") {
+      playGame("Paper");
+   } else if (event.key === "s") {
+      playGame("Scissors");
+   } else if (event.key === "l") {
+      playGame("Lizard");
+   } else if (event.key === "k") {
+      playGame("Spock");
+   }
+});
+
+updateScoreElement();
+
+function pickComputerMove() {
+   const randomNumber = Math.random();
+   let computerMove = "";
+
+   if (randomNumber >= 0 && randomNumber < 1 / 5) {
+      computerMove = "Rock";
+   } else if (randomNumber >= 1 / 5 && randomNumber < 2 / 5) {
+      computerMove = "Scissors";
+   } else if (randomNumber >= 2 / 5 && randomNumber < 3 / 5) {
+      computerMove = "Paper";
+   } else if (randomNumber >= 3 / 5 && randomNumber < 4 / 5) {
+      computerMove = "Lizard";
+   } else {
+      computerMove = "Spock";
+   }
+   return computerMove;
+}
+
+function playGame(playerMove) {
+   const computerMove = pickComputerMove();
+
+   let result = "";
+   if (playerMove === "Spock") {
+      if (computerMove === "Spock") {
+         result = "Tie.";
+      } else if (computerMove === "Paper" || computerMove === "Lizard") {
+         result = "Computer wins.";
+      } else if (computerMove === "Rock" || computerMove === "Scissors") {
+         result = "You win!";
+      }
+   } else if (playerMove === "Lizard") {
+      if (computerMove === "Lizard") {
+         result = "Tie.";
+      } else if (computerMove === "Scissors" || computerMove === "Rock") {
+         result = "Computer wins.";
+      } else if (computerMove === "Paper" || computerMove === "Spock") {
+         result = "You win!";
+      }
+   } else if (playerMove === "Scissors") {
+      if (computerMove === "Scissors") {
+         result = "Tie.";
+      } else if (computerMove === "Rock" || computerMove === "Spock") {
+         result = "Computer wins.";
+      } else if (computerMove === "Paper" || computerMove === "Lizard") {
+         result = "You win!";
+      }
+   } else if (playerMove === "Paper") {
+      if (computerMove === "Paper") {
+         result = "Tie.";
+      } else if (computerMove === "Scissors" || computerMove === "Lizard") {
+         result = "Computer wins.";
+      } else if (computerMove === "Rock" || computerMove === "Spock") {
+         result = "You win!";
+      }
+   } else if (playerMove === "Rock") {
+      if (computerMove === "Rock") {
+         result = "Tie.";
+      } else if (computerMove === "Paper" || computerMove === "Spock") {
+         result = "Computer wins.";
+      } else if (computerMove === "Scissors" || computerMove === "Lizard") {
+         result = "You win!";
+      }
+   }
+
+   if (result === "You win!") {
+      score.wins++;
+   } else if (result === "Computer wins.") {
+      score.losses++;
+   } else if (result === "Tie.") {
+      score.ties++;
+   }
+
+   localStorage.setItem("score", JSON.stringify(score));
+
+   updateScoreElement();
+
+   document.querySelector(".js-result").innerHTML = result;
+
+   document.querySelector(
+      ".js-moves"
+   ).innerHTML = `You <img src="images/${playerMove}.png" class="move-icon" alt="">VS.<img src="images/${computerMove}.png" class="move-icon" alt=""> Computer`;
+
+   /*alert(
+     `You picked ${playerMove}. Computer picked ${computerMove}. ${result}
+    Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
+  );*/
+}
+
+function updateScoreElement() {
+   document.querySelector(
+      ".js-score"
+   ).innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
---
+++ appPages/weather/scripts/index.js
@@ -0,0 +1,69 @@
+const apiKey = "0c9125ae479be95dda29f10ee7aee951";
+
+const weatherDataElement = document.getElementById("weather-data");
+
+const cityInputElement = document.getElementById("city-input");
+
+const formElement = document.querySelector("form");
+
+formElement.addEventListener("submit", (event) => {
+   event.preventDefault();
+   const cityValue = cityInputElement.value;
+   getWeatherData(cityValue);
+   // console.log(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+   try {
+      const response = await fetch(
+         `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`
+      );
+
+      if (!response.ok) {
+         throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const temperature = Math.round(data.main.temp);
+
+      const description = data.weather[0].description;
+
+      const icon = data.weather[0].icon;
+
+      const details = [
+         `Feels like: ${Math.round(data.main.feels_like)}°C`,
+         `Humidity: ${Math.round(data.main.humidity)}%`,
+         `Wind speed: ${Math.round(data.wind.speed)} m/s`,
+      ];
+
+      weatherDataElement.querySelector(".js-icon").innerHTML = `<img
+      src="http://openweathermap.org/img/wn/${icon}.png"
+      alt="Weather icon" />`;
+
+      weatherDataElement.querySelector(
+         ".js-temperature"
+      ).textContent = `${temperature}°C`;
+
+      weatherDataElement.querySelector(
+         ".js-description"
+      ).textContent = `${description}`;
+
+      weatherDataElement.querySelector(".js-details").innerHTML = details
+         .map((detail) => `<div>${detail}</div>`)
+         .join("");
+
+   } catch (error) {
+    weatherDataElement.querySelector(".js-icon").innerHTML = ""
+      weatherDataElement.querySelector(
+         ".js-temperature"
+      ).textContent = "";
+
+      weatherDataElement.querySelector(
+         ".js-description"
+      ).textContent = "An error ocurred. Please check the name of the city and try again :)";
+
+      weatherDataElement.querySelector(".js-details").innerHTML = ""
+
+   }
+}
